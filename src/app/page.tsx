ebd85273514c6e87388febcf1bd8ccfa6ad2c123
AKB4R03 <masakbar@gmail.com>
2024-03-22 21:14:01 +0700

    header mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,149 @@
+import { Inter } from "next/font/google";
 import Image from "next/image";
+const inter = Inter({ subsets: ["latin"] });
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <div
+      className={` ${inter} w-[364px] h-[735.50px] bg-gradient-to-b from-amber-500 to-amber-600 flex-col justify-start items-start inline-flex`}
+    >
+      <div className="self-stretch h-20  bg-white flex-col justify-center items-center flex">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          className="w-[167px] h-12"
+          width={500}
+          height={500}
+          src="/site-logo.png"
+          alt="site-logo"
+          style={{ objectFit: "cover" }}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-[364px] h-12 justify-center items-center gap-5 inline-flex">
+        <div className="text-white text-xs font-bold leading-none">Talent</div>
+        <div className="text-orange-200 text-xs font-normal leading-none">
+          Hiring Partner
+        </div>
       </div>
-    </main>
+      <div className="self-stretch h-[607.50px] px-5 pt-28 pb-10 flex-col justify-start items-start gap-10 flex">
+        <div className="w-[307px] h-[219px] relative">
+          {/* //* Hour training */}
+          <div className="w-[100px] h-[46px] px-3 py-2 left-[210px] top-[45px] z-10 absolute bg-gradient-to-r from-white to-orange-300 rounded-xl justify-start items-center gap-4 inline-flex">
+            <div className="flex-col justify-center items-start inline-flex">
+              <div className="text-yellow-900 text-sm font-bold leading-none ">
+                +500
+              </div>
+              <div className="text-amber-700 text-[10px] font-normal leading-[14px]">
+                Hour Training
+              </div>
+            </div>
+          </div>
+          {/* //* Talent Pool */}
+          <div className="w-[100px] h-[46px] px-3 py-2 right-[40px] top-[-80px] z-10 absolute bg-gradient-to-r from-white to-orange-300 rounded-xl justify-start items-center gap-4 inline-flex">
+            <div className="flex-col justify-center items-start inline-flex">
+              <div className="text-yellow-900 text-sm font-bold leading-none ">
+                +6000
+              </div>
+              <div className="text-amber-700 text-[10px] font-normal leading-[14px]">
+                Talent Pool
+              </div>
+            </div>
+          </div>
+          {/*//* intern full time */}
+          <div className="w-[109px] h-[46px] px-3 py-2 left-[30px] top-[90px] z-10 absolute bg-gradient-to-l from-white to-orange-300 rounded-xl justify-start items-center gap-4 inline-flex">
+            <div className="flex-col justify-center items-end inline-flex">
+              <div className="text-yellow-900 text-sm font-bold leading-none ">
+                80%
+              </div>
+              <div className="text-amber-700 text-[10px] font-normal leading-[14px]">
+                Intern To FullTime
+              </div>
+            </div>
+          </div>
+          {/*//* start up partners */}
+          <div className="w-[100px] h-[46px] px-3 py-2 left-[5px] top-[-50px] z-10 absolute bg-gradient-to-l from-white to-orange-300 rounded-xl justify-start items-center gap-4 inline-flex">
+            <div className="flex-col justify-center items-end inline-flex">
+              <div className="text-yellow-900 text-sm font-bold leading-none ">
+                +120
+              </div>
+              <div className="text-amber-700 text-[10px] font-normal leading-[14px]">
+                Startup Partner
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[195.50px] flex-col justify-start items-start flex">
+          <div className="w-[324px] relative">
+            <div className="w-[324px] h-[195.50px] z-0 bottom-[-35px] absolute flex-col justify-center items-center gap-2.5 inline-flex">
+              <Image
+                className=" w-full"
+                width={500}
+                height={500}
+                src="/frame 4.svg"
+                alt="frame 2"
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[164px] flex-col justify-start items-start gap-5 flex">
+          <div className="self-stretch z-10">
+            <span className="text-white text-[40px] font-black leading-[48px]">
+              JUMPSTART YOUR{" "}
+            </span>
+            <span className="text-white text-[40px] font-black leading-[48px]">
+              CAREER
+            </span>
+          </div>
+          <div className="self-stretch z-10 text-orange-200 text-xl font-bold leading-normal">
+            Govokasi help you to growth and gain your full potential skills
+          </div>
+          <div className="mt-6 z-10">
+            <div className=" px-5 p-4 bg-white rounded-[999px] justify-center items-center gap-4 inline-flex">
+              <div className="text-slate-950 text-sm font-bold leading-none">
+                Register Now
+              </div>
+              <div>
+                {" "}
+                <ArrowRightIcon className="h-6 w-6 text-black" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[307px] h-[219px] relative">
+          <Image
+            className="absolute z-0 bottom-[-40px] right-[-30px]"
+            width={200}
+            height={200}
+            src="/vector 2.svg"
+            alt="site-logo"
+            style={{ objectFit: "cover" }}
+          />
+          <Image
+            className="absolute h-min z-0 bottom-[-40px] right-[-30px]"
+            width={100}
+            height={200}
+            src="/vector 3.svg"
+            alt="site-logo"
+            style={{ objectFit: "cover" }}
+          />
+          <Image
+            className="absolute h-min z-0 bottom-[140px] right-[230px]"
+            width={45}
+            height={43}
+            src="/star 2.svg"
+            alt="big star"
+            style={{ objectFit: "cover" }}
+          />
+          <Image
+            className="absolute h-min z-0 bottom-[90px] right-[200px]"
+            width={22.28}
+            height={21.31}
+            src="/star 3.svg"
+            alt="little star"
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
