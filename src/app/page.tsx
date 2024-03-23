d56497b547a1d46301e9cc03f99276be89e3522c
Babang <46173577+bagasysf@users.noreply.github.com>
2024-03-24 04:22:43 +0700

    Slicing header mobile & dekstop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Services from "@/app/components/Services";
-import Header from "./components/header";
-import CompanyValues from "@/app/components/CompanyValues";
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+import Services from '@/app/components/Services';
+import Header from './components/Header';
+import CompanyValues from '@/app/components/CompanyValues';
+// import { Inter } from 'next/font/google';
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className={`${inter} flex min-h-screen flex-col`}>
+    <main className={`flex flex-col`}>
       <div className="items-center mx-auto">
         <Header />
         <Services />
